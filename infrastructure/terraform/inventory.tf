87fb88c6344ca050d5c24ff08deaa006e6c477ba
gregwpr <greg@telkab.pl>
2021-11-25 17:55:13 +0000

    eee

--- infrastructure/terraform/inventory.tf
+++ infrastructure/terraform/inventory.tf
@@ -1,4 +1,5 @@
 resource "local_file" "ansible_inventory"
  { content = templatefile("inventory.tpl",
- { ansible_ip = "${join("\n", aws_instance.panda.*.public_ip)}" }
-	) filename = "${path.module}/../ansible/inventory" }
+   ansible_ip = "${join("\n", aws_instance.panda.*.public_ip)}" 
+	)
+	filename = "${path.module}/../ansible/inventory" }
